026c49f41a927ece6c3ade8aee0453a59c4b2334
Diego620 <diegocupido25@gmail.com>
2025-07-29 12:02:15 +0200

    Full project setup

--- server/Controllers/WeatherForecastController.cs
+++ server/Controllers/WeatherForecastController.cs
@@ -3,31 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 namespace server.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    [Route("hello")]
+    public class HelloController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        [HttpGet]
+        public string Get()
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "Hello World from the backend!";
         }
     }
 }
